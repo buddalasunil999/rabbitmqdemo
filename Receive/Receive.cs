ca2812a2d3b81231f2a758118329a24b54d0b41d
Sunil Kumar Reddy Buddala <buddalasunil999@gmail.com>
2018-11-27 11:58:59 +1300

    Release 3.0 (#5)
    * Myfeature (#1)
    
    * remove queue
    
    * consumer
    
    * Bumped version number to 1.2 (#3)
    
    * refactored receiver (#4)
    
    * new version

--- Receive/Receive.cs
+++ Receive/Receive.cs
@@ -1,42 +1,10 @@
-﻿using System;
-using System.Text;
-using RabbitMQ.Client;
-using RabbitMQ.Client.Events;
-
-namespace Receive
+﻿namespace Receive
 {
     class Program
     {
         static void Main(string[] args)
         {
-            var factory = new ConnectionFactory { HostName = "localhost" };
-            using (var connection = factory.CreateConnection())
-            {
-                using (var channel = connection.CreateModel())
-                {
-                    channel.QueueDeclare(queue: "hello",
-                        durable: false,
-                        exclusive: false,
-                        autoDelete: false,
-                        arguments: null);
-
-                    var consumer = new EventingBasicConsumer(channel);
-                    consumer.Received += Consumer_Received;
-
-                    channel.BasicConsume(queue: "hello",
-                        autoAck: true,
-                        consumer: consumer);
-
-                    Console.ReadKey();
-                }
-            }
-        }
-
-        private static void Consumer_Received(object sender, BasicDeliverEventArgs e)
-        {
-            var body = e.Body;
-            var message = Encoding.UTF8.GetString(body);
-            Console.WriteLine($" [x] Received {message}");
+            new Receiver().Start();
         }
     }
 }
---
+++ Receive/Receiver.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Text;
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+
+namespace Receive
+{
+    public class Receiver
+    {
+        public void Start()
+        {
+            var factory = new ConnectionFactory { HostName = "localhost" };
+            using (var connection = factory.CreateConnection())
+            {
+                using (var channel = connection.CreateModel())
+                {
+                    channel.QueueDeclare("hello",
+                        durable: false,
+                        exclusive: false,
+                        autoDelete: false,
+                        arguments: null);
+
+                    var consumer = new EventingBasicConsumer(channel);
+                    consumer.Received += Consumer_Received;
+
+                    channel.BasicConsume("hello",
+                        true,
+                        consumer);
+
+                    Console.ReadKey();
+                }
+            }
+        }
+
+        private static void Consumer_Received(object sender, BasicDeliverEventArgs e)
+        {
+            var body = e.Body;
+            var message = Encoding.UTF8.GetString(body);
+            Console.WriteLine($" [x] Received {message}");
+        }
+    }
+}
